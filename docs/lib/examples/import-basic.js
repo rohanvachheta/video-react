cbe5f2bf3c024eed2c224952caea1b0ad65fadea
rohan <rohan.vachheta@marutitech.com>
2021-01-22 18:53:07 +0530

    fix : document object

--- docs/lib/examples/import-basic.js
+++ docs/lib/examples/import-basic.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Player } from 'video-react';
+import { Player, Shortcut } from 'video-react';
 
 export default props => {
   return (
     <Player
       playsInline
-      poster="/assets/poster.png"
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+      src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
       document={window.document}
-    />
+      disabledFullscreens
+    >
+      <Shortcut
+        dblclickable
+        clickable
+        handleFullscreenFun={() => console.log('rohan123')}
+      />
+    </Player>
   );
 };
